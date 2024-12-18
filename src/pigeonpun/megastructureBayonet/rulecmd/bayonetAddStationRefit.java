4b8ff6e23c7c58c2dfec7b99eda559ef8e4fce66
CosmicFishi <lyquocdung099@gmail.com>
2024-12-18 16:58:17 -0600

    More stuffs, station refit seems to be station, need more testing

--- src/pigeonpun/megastructureBayonet/rulecmd/bayonetAddStationRefit.java
+++ src/pigeonpun/megastructureBayonet/rulecmd/bayonetAddStationRefit.java
@@ -1,9 +1,14 @@
 package pigeonpun.megastructureBayonet.rulecmd;
 
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
 import com.fs.starfarer.api.util.Misc;
+import pigeonpun.megastructureBayonet.ModPlugin;
+import pigeonpun.megastructureBayonet.structure.bayonetManager;
 
 import java.util.List;
 import java.util.Map;
@@ -12,6 +17,14 @@ public class bayonetAddStationRefit extends BaseCommandPlugin {
     @Override
     public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
         //todo: add station for refit
+        if(dialog.getInteractionTarget() != null && dialog.getInteractionTarget() instanceof CampaignFleetAPI && dialog.getInteractionTarget().getMemoryWithoutUpdate().get(bayonetManager.bayonet_entity_ID) != null) {
+            CampaignFleetAPI fleet = (CampaignFleetAPI) dialog.getInteractionTarget();
+            if(fleet.getFleetData() != null && !fleet.getFleetData().getMembersListCopy().isEmpty()) {
+                FleetMemberAPI bayonetStation = ((CampaignFleetAPI) dialog.getInteractionTarget()).getFleetData().getMembersListCopy().get(0);
+                //adding it to player fleet
+                Global.getSector().getPlayerFleet().getFleetData().addFleetMember(bayonetStation);
+            }
+        }
         return false;
     }
 }
---
+++ src/pigeonpun/megastructureBayonet/rulecmd/bayonetCleanUpAfterStationRefit.java
@@ -0,0 +1,36 @@
+package pigeonpun.megastructureBayonet.rulecmd;
+
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.campaign.FleetDataAPI;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.impl.campaign.rulecmd.BaseCommandPlugin;
+import com.fs.starfarer.api.util.Misc;
+import org.apache.log4j.Logger;
+
+import java.util.List;
+import java.util.Map;
+
+public class bayonetCleanUpAfterStationRefit extends BaseCommandPlugin {
+    public static Logger log = Global.getLogger(bayonetCleanUpAfterStationRefit.class);
+    @Override
+    public boolean execute(String ruleId, InteractionDialogAPI dialog, List<Misc.Token> params, Map<String, MemoryAPI> memoryMap) {
+        CampaignFleetAPI fleet = (CampaignFleetAPI) dialog.getInteractionTarget();
+        if(fleet.getFleetData() != null && !fleet.getFleetData().getMembersListCopy().isEmpty()) {
+            FleetMemberAPI bayonetStation_Origin = ((CampaignFleetAPI) dialog.getInteractionTarget()).getFleetData().getMembersListCopy().get(0);
+            FleetMemberAPI bayonetStation_Modified;
+            FleetDataAPI playerFleetData = Global.getSector().getPlayerFleet().getFleetData();
+            for(FleetMemberAPI member: playerFleetData.getMembersListCopy()) {
+                if(member.getId().equals(bayonetStation_Origin.getId())) {
+                    playerFleetData.removeFleetMember(member);
+                    log.info("Cleaned up after Bayonet refit");
+                    bayonetStation_Modified = member;
+                    break;
+                }
+            }
+        }
+        return false;
+    }
+}
